03428be34055a6c7cc3db9ddb06a7671221ab069
Felipe Coury <felipe.coury@gmail.com>
2011-12-14 18:45:27 -0200

    Open target and spec side by side

--- GoToSpec.py
+++ GoToSpec.py
@@ -1,7 +1,31 @@
 import sublime, sublime_plugin, os
 
 class GoToSpecCommand(sublime_plugin.WindowCommand):
-	def open_spec(self, folder, dirname, filename, extension):
+	def open_left(self, file):
+		self.window.open_file(file)
+		if self.window.active_group() == 0:
+			return
+
+		self.window.run_command('close')
+		self.window.run_command('set_layout', 
+			{ "cols": [0.0, 0.5, 1.0], "rows": [0.0, 1.0], "cells": [[0,0,1,1],[1,0,2,1]] })
+
+		self.window.focus_group(0)
+		self.window.open_file(file)
+
+	def open_right(self, file):
+		self.window.open_file(file)
+		if self.window.active_group() == 1:
+			return
+
+		self.window.run_command('close')
+		self.window.run_command('set_layout', 
+			{ "cols": [0.0, 0.5, 1.0], "rows": [0.0, 1.0], "cells": [[0,0,1,1],[1,0,2,1]] })
+
+		self.window.focus_group(1)
+		self.window.open_file(file)
+
+	def find_spec(self, folder, dirname, filename, extension):
 		if dirname.startswith('/app'):
 			dirname = dirname[4:]
 
@@ -9,23 +33,32 @@ class GoToSpecCommand(sublime_plugin.WindowCommand):
 		filename = filename + "_spec" + extension
 
 		spec_file = folder + dirname + filename
-
 		if os.path.isfile(spec_file):
-			self.window.open_file(spec_file)
+			return spec_file
 
-	def open_test_subject(self, folder, dirname, filename, extension):
+		dirname = dirname.replace('spec/', 'spec/requests/')
+
+		spec_file = folder + dirname + filename
+		if os.path.isfile(spec_file):
+			return spec_file
+
+	def find_test_subject(self, folder, dirname, filename, extension):
 		filename = filename[0:-5]
-		dirname = dirname[5:]
+		dirname = dirname[5:] + '/'
 
-		test_subject = folder + dirname + '/' + filename + extension
+		test_subject = folder + dirname + filename + extension
 		if os.path.isfile(test_subject):
-			self.window.open_file(test_subject)
-			return
-		
-		test_subject = folder + '/app' + dirname + '/' + filename + extension
+			return test_subject
+
+		dirname = '/app' + dirname
+		test_subject = folder + dirname + filename + extension
 		if os.path.isfile(test_subject):
-			self.window.open_file(test_subject)
-			return
+			return test_subject
+
+		dirname = dirname.replace('/requests', '')
+		test_subject = folder + dirname + filename + extension
+		if os.path.isfile(test_subject):
+			return test_subject
 
 	def run(self):
 		view = self.window.active_view()
@@ -42,9 +75,20 @@ class GoToSpecCommand(sublime_plugin.WindowCommand):
 		filename, extension = os.path.splitext(filename)
 
 		if filename.endswith('_spec'):
-			self.open_test_subject(folder, dirname, filename, extension)
+			spec_file    = current_folder + current_file
+			subject_file = self.find_test_subject(folder, dirname, filename, extension)
+
+			if spec_file and subject_file:
+				self.open_left(spec_file)
+				self.open_right(subject_file)
 		else:
-			self.open_spec(folder, dirname, filename, extension)
+			spec_file    = self.find_spec(folder, dirname, filename, extension)
+			subject_file = current_folder + current_file
+
+			if spec_file and subject_file:
+				self.open_right(subject_file)
+				self.open_left(spec_file)
+
 
 
 		
